6083c8dcbeb3609cdf73e2937266c880df8315c4
degendevv <degendevv@protonmail.com>
2021-12-09 17:07:16 +0000

    improve mobile responsiveness

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,14 +5,14 @@ import Image from 'next/image'
 const Header: React.FC = () => {
   return (
     <header>
-      <nav className="flex items-center flex-wrap p-6">
+      <nav className="flex flex-col lg:flex-row items-center flex-wrap p-6">
         <Link href="/" passHref>
           <a className="inline-flex items-center p-2 mr-4">
             <Image className="rounded-full" src="/logo.jpg" alt="Pepe Le Krabs" width={150} height={150} />
           </a>
         </Link>
-        <h1 className="py-10 mx-20 text-4xl font-bold">What could Mongoose do to Crypto Coin?</h1>
-        <div className='lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto'>
+        <h1 className="text-center flex-grow py-10 text-4xl font-bold">What could Mongoose do to Crypto Coin?</h1>
+        <div className="text-center lg:inline-flex lg:flex-row lg:ml-auto lg:w-auto w-full lg:items-center items-start flex flex-col lg:h-auto">
           <a href="https://traderjoexyz.com/#/trade?outputCurrency=0xd74cca216f7c8fe33f0911da8698e2b3efc98835" className='lg:inline-flex lg:w-auto w-full px-3 py-2 rounded font-bold items-center justify-center bg-red-700 hover:bg-red-900 text-white'>
             Buy $MONG
           </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -17,24 +17,20 @@ const Home: NextPage = () => {
       <Header />
       <div className="flex flex-col items-center justify-center min-h-screen py-2">
         <Blurbs />
-        <main className="flex flex-col items-center justify-center w-full flex-1 my-20 px-20 text-center">
+        <main className="flex flex-col items-center justify-center w-full flex-1 mt-20 px-5 md:px-20 text-center">
           <h1 className="text-6xl font-bold">
             The First Mongoose Crypto Coin!
           </h1>
 
-        <div className="flex flex-row items-center justify-center">
-          <div className="p-10 w-screen">
+        <div className="flex flex-row items-center justify-center w-full">
+          <div className="pt-20 w-full">
             <IFrame
               title="Trade on Traderjoe"
               src={"https://traderjoexyz.com/#/trade?outputCurrency=0xd74cca216f7c8fe33f0911da8698e2b3efc98835"}
             />
           </div>
-        
         </div>
-
-          
         </main>
-
         <Footer />
       </div>
     </div>
